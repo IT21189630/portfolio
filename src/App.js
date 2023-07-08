c04a98b88f910a10bf466d941678943e448a29ae
IT21189630 <it21189630@my.sliit.lk>
2023-07-08 15:07:38 +0530

    first build

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ const App = () => {
         <HeroSection/>
         <ProfileSection/>
         <SkillSection/>
-        <ProjectSection/>
-        <ContactSection/>
+        {/* <ProjectSection/>
+        <ContactSection/> */}
     </div>
   )
 
